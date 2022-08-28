1d2f925c0eb5763a1acee3e7534534b4a9d34ac3
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2022-08-28 01:28:37 -0600

    Create nametable dumper

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 #include "cpu.h"
 #include "nes.h"
+#include "rom.h"
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
 
 int main() {
+    const char game[] = "tests/roms/nestest.nes";
+    auto mapper       = nes::LoadRomFile(game);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "could not initialize sdl2" << SDL_GetError();
         return 1;
     }
 
-    auto window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                   SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    const auto bitsPerPx     = 4;
+    const auto imagesPerLine = 32;
+    const auto numImages     = mapper->cartridge->chrROM.size() / 16;
+    const auto imagesTall    = numImages / imagesPerLine;
+
+    auto width               = 1 + (1 + 8 * bitsPerPx) * imagesPerLine;
+    auto height              = 1 + (1 + 8 * bitsPerPx) * imagesTall;
+
+    auto window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height,
+                                   SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cout << "could not create window" << SDL_GetError();
         return 2;
@@ -26,9 +36,36 @@ int main() {
         return 3;
     }
 
-    if (SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00)) < 0) {
-        std::cout << "could not draw on window" << SDL_GetError();
-        return 4;
+    uint32_t *pixel32 = static_cast<uint32_t *>(screenSurface->pixels);
+    auto &chr         = mapper->cartridge->chrROM;
+
+    for (int img = 0; img < numImages; img++) {
+        for (int y = 0; y < 8; y++) {
+            auto imgCol = img % imagesPerLine;
+            auto imgRow = img / imagesPerLine;
+
+            for (int x = 0; x < 8; x++) {
+                // 16 bytes per tile = 128 bits
+                // planeOne = offset + [0 ... 7]
+                // planeTwo = planeOne + 8
+                auto tileStartByte = img * 16;
+                auto planeOne      = (chr[tileStartByte + y] >> (7 - x)) & 1;
+                auto planeTwo      = (chr[tileStartByte + y + 8] >> (7 - x)) & 1;
+
+                auto startX        = 1 + imgCol * (1 + bitsPerPx * 8) + bitsPerPx * x;
+                auto startY        = 1 + imgRow * (1 + bitsPerPx * 8) + bitsPerPx * y;
+
+                // convert coordinates to the screen
+                auto intensity = planeOne << 7 | planeTwo << 6;
+                auto color     = SDL_MapRGBA(screenSurface->format, intensity, intensity, intensity, 0xff);
+
+                for (auto dx = 0; dx < bitsPerPx; dx++) {
+                    for (auto dy = 0; dy < bitsPerPx; dy++) {
+                        pixel32[(startY + dy) * width + (startX + dx)] = color;
+                    }
+                }
+            }
+        }
     }
 
     if (SDL_UpdateWindowSurface(window) < 0) {
@@ -41,12 +78,6 @@ int main() {
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-            if (e.type == SDL_KEYDOWN) {
-                quit = true;
-            }
-            if (e.type == SDL_MOUSEBUTTONDOWN) {
                 quit = true;
             }
         }
